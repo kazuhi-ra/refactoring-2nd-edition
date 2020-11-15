8428c9dca4d91ce56f1eb422d0584a13e0ad89bd
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-15 23:14:44 +0900

    型の名前を引数に入れ込む

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -13,22 +13,22 @@ const playsExample = {
   othello: { name: 'Othello', type: 'tragedy' },
 }
 
-function amountFor(performance, play) {
+function amountFor(aPerformance, play) {
   let result = 0
 
   switch (play.type) {
     case 'tragedy':
       result = 40000
-      if (performance.audience > 30) {
-        result += 10000 + 500 * (performance.audience - 30)
+      if (aPerformance.audience > 30) {
+        result += 10000 + 500 * (aPerformance.audience - 30)
       }
       break
     case 'comedy':
       result = 30000
-      if (performance.audience > 20) {
-        result += 10000 + 500 * (performance.audience - 20)
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20)
       }
-      result += 300 * performance.audience
+      result += 300 * aPerformance.audience
       break
     default:
       throw new Error(`unknown type: ${play.type}`)
