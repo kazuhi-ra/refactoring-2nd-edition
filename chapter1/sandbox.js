c4d41cf67e9ba3e909d531449f6ad9f93d05a05d
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-17 00:18:56 +0900

    「変数のインライン化」

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -81,10 +81,8 @@ function statement(invoice = invoiceExample, plays = playsExample) {
     totalAmount += amountFor(performance)
   }
 
-  let volumeCredits = totalVolumeCredits()
-
   result += `Amount owed is ${usd(totalAmount)}\n`
-  result += `You earned ${volumeCredits} credits\n`
+  result += `You earned ${totalVolumeCredits()} credits\n`
   return result
 }
 
