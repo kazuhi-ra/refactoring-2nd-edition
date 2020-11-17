a7756841756eb2124453a23a34b133aa24ba5f6d
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-17 23:57:06 +0900

    amountもstatement側に移動完了

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -23,12 +23,37 @@ function statement(invoice = invoiceExample, plays = playsExample) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
+    result.amount = amountFor(result)
 
     return result
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0
+
+    switch (aPerformance.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 10000 + 500 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`unknown type: ${aPerformance.play.type}`)
+    }
+
+    return result
   }
 }
 
@@ -37,7 +62,7 @@ function renderPlainText(data, plays) {
 
   for (let performance of data.performances) {
     // 注文の内訳を出力
-    result += `${performance.play.name}: ${usd(amountFor(performance))} (${
+    result += `${performance.play.name}: ${usd(performance.amount)} (${
       performance.audience
     } seats)\n`
   }
@@ -101,7 +126,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0
     for (let performance of data.performances) {
-      result += amountFor(performance)
+      result += performance.amount
     }
 
     return result
