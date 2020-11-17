cc2cdfc464cb4c1e8a11d3285c7e63ade2e16ee2
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-18 01:25:52 +0900

    HTMLを生成する版が完成

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -15,8 +15,6 @@ const playsExample = {
   othello: { name: 'Othello', type: 'tragedy' },
 }
 
-
-
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`
 
@@ -40,4 +38,30 @@ function renderPlainText(data) {
   }
 }
 
-console.log(renderPlainText(createStatementData(invoiceExample, playsExample)))
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`
+  result += '<table>\n'
+
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>'
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`
+    result += `<td>${usd(perf.amount)}</td></tr>\n`
+  }
+
+  result += '</table>\n'
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>`
+
+  return result
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100)
+  }
+}
+
+// console.log(renderPlainText(createStatementData(invoiceExample, playsExample)))
+console.log(renderHtml(createStatementData(invoiceExample, playsExample)))
