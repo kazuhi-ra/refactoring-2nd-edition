8fab53cc829b408b8d980fd178a21f10884ea704
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-18 21:47:56 +0900

    変数のインライン化

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -25,16 +25,14 @@ export function statement(invoice = invoiceExample, plays = playsExample) {
   }).format
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf)
-
     volumeCredits += Math.max(perf.audience - 30, 0)
     if (playFor(perf).type === 'comedy')
       volumeCredits += Math.floor(perf.audience / 5)
 
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`
-    totalAmount += thisAmount
+    totalAmount += amountFor(perf)
   }
 
   result += `Amount owed is  ${format(totalAmount / 100)}\n`
