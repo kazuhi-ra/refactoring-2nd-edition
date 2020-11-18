5229f1592b43330c51b8a4deb96c68bb1d18b621
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-18 14:42:20 +0900

    一から写経し直した

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -2,22 +2,21 @@ const invoiceExample = {
   customer: 'BigCo',
   performances: [
     { playID: 'hamlet', audience: 55 },
-    { playID: 'as−like', audience: 35 },
+    { playID: 'as-like', audience: 35 },
     { playID: 'othello', audience: 40 },
   ],
 }
 
 const playsExample = {
   hamlet: { name: 'Hamlet', type: 'tragedy' },
-  'as−like': { name: 'As You Like It', type: 'comedy' },
+  'as-like': { name: 'As You Like It', type: 'comedy' },
   othello: { name: 'Othello', type: 'tragedy' },
 }
 
-
 export function statement(invoice = invoiceExample, plays = playsExample) {
-  let totalAmount = 0
-  let volumeCredits = 0
-  let result = `Statement for ${invoice.customer}\n`
+  let totalAmount = 0 // 合計金額
+  let volumeCredits = 0 // ボリューム特典のポイント
+  let result = `Statement for ${invoice.customer}\n` //
 
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -25,41 +24,38 @@ export function statement(invoice = invoiceExample, plays = playsExample) {
     minimumFractionDigits: 2,
   }).format
 
-  for (let performance of invoice.performances) {
-    const play = plays[performance.playID]
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID]
     let thisAmount = 0
 
     switch (play.type) {
       case 'tragedy':
         thisAmount = 40000
-        if (performance.audience > 30) {
-          thisAmount += 10000 + 500 * (performance.audience - 30)
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30)
         }
         break
       case 'comedy':
         thisAmount = 30000
-        if (performance.audience > 20) {
-          thisAmount += 10000 + 500 * (performance.audience - 20)
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20)
         }
-        thisAmount += 300 * performance.audience
+        thisAmount += 300 * perf.audience
         break
       default:
         throw new Error(`unknown type: ${play.type}`)
     }
 
-    // ボリューム特典のポイントの加算
-    volumeCredits += Math.max(performance.audience - 30, 0)
-    // comedyのときは、10人につきさらにポイントを加算
-    if ('comedy' === play.type)
-      volumeCredits += Math.floor(performance.audience / 5)
-    // 注文の内訳を出力
-    result += `${play.name}: ${format(thisAmount / 100)} (${
-      performance.audience
+    volumeCredits += Math.max(perf.audience - 30, 0)
+    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
+
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
     } seats)\n`
     totalAmount += thisAmount
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `Amount owed is  ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
 }
--- test/sandbox.js
+++ test/sandbox.js
@@ -3,10 +3,10 @@ import assert from 'assert'
 import { statement } from '../chapter1/sandbox.js'
 
 const output = `Statement for BigCo
-Hamlet: $625.00 (55 seats)
-As You Like It: $580.00 (35 seats)
-Othello: $550.00 (40 seats)
-Amount owed is $1,755.00
+ Hamlet: $650.00 (55 seats)
+ As You Like It: $580.00 (35 seats)
+ Othello: $500.00 (40 seats)
+Amount owed is  $1,730.00
 You earned 47 credits\n`
 
 try {
