99e2823f3f0b72dd8abdcfc70f048933059118d3
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-19 01:09:41 +0900

    関数の抽出

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -14,15 +14,15 @@ const playsExample = {
 }
 
 export function statement(invoice = invoiceExample, plays = playsExample) {
-  let totalAmount = 0 // 合計金額
   let result = `Statement for ${invoice.customer}\n` //
 
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`
-    totalAmount += amountFor(perf)
   }
+
+  let totalAmount = appleSauce()
 
   result += `Amount owed is  ${usd(totalAmount)}\n`
   result += `You earned ${totalVolumeCredits()} credits\n`
@@ -79,6 +79,15 @@ export function statement(invoice = invoiceExample, plays = playsExample) {
       volumeCredits += volumeCreditsFor(perf)
     }
     return volumeCredits
+  }
+
+  function appleSauce() {
+    let totalAmount = 0 // 合計金額
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf)
+    }
+
+    return totalAmount
   }
 }
 
