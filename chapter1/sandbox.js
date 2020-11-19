d21c14a14f07d5422aad479db3ae443307e1acb7
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-20 02:27:26 +0900

    引数playを引き剥がせた

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -17,17 +17,28 @@ const playsExample = {
 export function statement(invoice = invoiceExample, plays = playsExample) {
   const statementData = {}
   statementData.customer = invoice.customer
-  statementData.performances = invoice.performances
+  statementData.performances = invoice.performances.map(enrichPerformance)
 
-  return renderPlainText(statementData, plays)
+  return renderPlainText(statementData)
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance)
+    result.play = playFor(aPerformance)
+
+    return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
 }
 
 // 引数dataで渡されたデータを加工するだけの関数にしたい
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n` //
 
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`
   }
@@ -39,7 +50,7 @@ function renderPlainText(data, plays) {
   function amountFor(aPerformance) {
     let result = 0
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy':
         result = 40000
         if (aPerformance.audience > 30) {
@@ -54,20 +65,16 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience
         break
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+        throw new Error(`unknown type: ${aPerformance.play.type}`)
     }
 
     return result
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-  }
-
   function volumeCreditsFor(aPerformance) {
     let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
-    if (playFor(aPerformance).type === 'comedy')
+    if (aPerformance.play.type === 'comedy')
       result += Math.floor(aPerformance.audience / 5)
 
     return result
