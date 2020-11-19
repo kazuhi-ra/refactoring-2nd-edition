b5868702b1670597b62d90e1f1bc4b1f4f2e85a7
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-19 16:18:11 +0900

    中間データとなるオブジェクトを用意

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -14,10 +14,11 @@ const playsExample = {
 }
 
 export function statement(invoice = invoiceExample, plays = playsExample) {
-  return renderPlainText(invoice, plays)
+  const statementData = {}
+  return renderPlainText(statementData, invoice, plays)
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${invoice.customer}\n` //
 
   for (let perf of invoice.performances) {
