026367241250970cb6299e0e3581e3f8be602326
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-20 06:40:28 +0900

    「パイプラインによるループの置き換え」

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -70,20 +70,11 @@ export function statement(invoice = invoiceExample, plays = playsExample) {
   }
 
   function totalVolumeCredits(data) {
-    let result = 0 // ボリューム特典のポイント
-    for (let perf of data.performances) {
-      result += perf.volumeCredits
-    }
-    return result
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
   }
 
   function totalAmount(data) {
-    let result = 0 // 合計金額
-    for (let perf of data.performances) {
-      result += perf.amount
-    }
-
-    return result
+    return data.performances.reduce((total, p) => total + p.amount, 0)
   }
 }
 
