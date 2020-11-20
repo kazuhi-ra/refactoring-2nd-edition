e60ca2d5cfd75276c00105019d27387e4a378326
kazuhi-ra <kazuhira4126@gmail.com>
2020-11-20 23:54:38 +0900

    「関数のインライン化」

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -16,7 +16,7 @@ export function createStatementData(invoice, plays) {
     )
     const result = Object.assign({}, aPerformance)
     result.play = calculator.play
-    result.amount = amountFor(result)
+    result.amount = calculator.amount
     result.volumeCredits = volumeCreditsFor(result)
 
     return result
@@ -24,31 +24,6 @@ export function createStatementData(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
-    // let result = 0
-
-    // switch (aPerformance.play.type) {
-    //   case 'tragedy':
-    //     result = 40000
-    //     if (aPerformance.audience > 30) {
-    //       result += 1000 * (aPerformance.audience - 30)
-    //     }
-    //     break
-    //   case 'comedy':
-    //     result = 30000
-    //     if (aPerformance.audience > 20) {
-    //       result += 10000 + 500 * (aPerformance.audience - 20)
-    //     }
-    //     result += 300 * aPerformance.audience
-    //     break
-    //   default:
-    //     throw new Error(`unknown type: ${aPerformance.play.type}`)
-    // }
-
-    // return result
   }
 
   function volumeCreditsFor(aPerformance) {
